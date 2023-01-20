26989fefb6f155340b5efba4df239659a603bd25
Felicia DiPietro <95667697+feliciadipi@users.noreply.github.com>
2023-01-20 16:40:00 -0500

    outline pomodoro timer module
    Implement basic timer functionality in a Timer class. Separated timer and clock into separate modules, both imported and rendered from main.js

--- client/scripts/clock.js
+++ client/scripts/clock.js
@@ -1,7 +1,3 @@
-import state from "./state.js"; // Singleton
-
-/*------------------------Clock------------------------*/
-
 function refreshClock() {
   const clock = new Date();
   return clock;
@@ -29,50 +25,4 @@ function startClock(element) {
   setInterval(renderClock(element), 1000);
 }
 
-/*-----------------------Timer-------------------------*/
-
-// TODO
-function renderTimer(element) {
-  element.innerHTML = '';
-
-  const timer = document.createElement('div');
-  timer.id = 'timer';
-  timer.innerText = 'TIMER'; 
-  timer.classList.add('mt-5');
-  element.appendChild(timer);
-
-  const controls = document.createElement('div');
-  controls.id = 'timer-controls';
-  controls.classList.add('d-inline-flex', 'flex-row', 'justify-content-center', 'mb-3');
-
-  const playPauseButton = document.createElement('a');
-  playPauseButton.classList.add('btn', 'mx-1');
-  playPauseButton.href = '#';
-  playPauseButton.innerText = '▶';
-  controls.appendChild(playPauseButton);
-
-  const resetButton = document.createElement('a');
-  resetButton.classList.add('btn', 'mx-1');
-  resetButton.href = '#';
-  resetButton.innerText = '↺';
-  controls.appendChild(resetButton);
-
-  element.appendChild(controls);
-}
-
-/*------------------------Render------------------------*/
-
-const clockContainer = document.getElementById('clock-container');
-startClock(clockContainer);
-
-const modeButton = document.getElementById('mode-button');
-modeButton.addEventListener('click', () => {
-  const mode = state.toggle('pomodoro');
-  if (mode) {
-    modeButton.innerText = 'Timer ⏳';
-    renderTimer(clockContainer);
-  } else {
-    modeButton.innerText = 'Clock 🕰️';
-    startClock(clockContainer);
-  }
-});
+export default startClock;
