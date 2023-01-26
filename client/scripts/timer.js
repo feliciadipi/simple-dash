69a2131903c8290a95d2de524d93d510d1ca55b3
Felicia DiPietro <95667697+feliciadipi@users.noreply.github.com>
2023-01-26 16:14:30 -0500

    draft input timer view

--- client/scripts/timer.js
+++ client/scripts/timer.js
@@ -1,58 +1,81 @@
+/*---------- Timer Class ----------*/
+
 class Timer {
-  constructor(display) {
-    this.display = display;
-    this.timestamp = undefined;
-  }
+  constructor(element, duration) {
 
-  start(duration) {
-  
-    let min = duration-1;
-    let sec = 59;
-    let timer = setInterval(() => {
-      if (sec > 9) {
-        this.display.innerText = min+':'+sec;
+    const display = element;
+    const time = {min: duration-1, sec: 59};
+    let paused = true;
+    let interval = undefined;
+
+    const countdown = () => {
+      if (time.sec > 9) {
+        display.innerText = time.min+':'+time.sec;
       } else {
-        this.display.innerText = min+':0'+sec;
+        display.innerText = time.min+':0'+time.sec;
       }
-      --sec;
-      if (sec <= 0) {
-        if (min <= 0) {
-          clearInterval(timer);
-          this.display.innerText = '0:00';
+      --time.sec;
+      if (time.sec <= 0) {
+        if (time.min <= 0) {
+          clearInterval(interval);
+          display.innerText = '0:00';
         } else {
-          sec = 59;
-          --min;
+          time.sec = 59;
+          --time.min;
         }
+      } 
+    };
+
+    this.start = function() {
+      if (paused) {
+        interval = setInterval(countdown, 1000);
+        paused = false;
       }
-    }, 1000);
-  }
+    }
 
-  pause() {
-
-  }
-
-  resume() {
-
+    this.pause = function() {
+      if (!paused) {
+        clearInterval(interval);
+        interval = undefined;
+        paused = true;
+      }
+    }
   }
 }
+
+/*---------- Input View ----------*/
 
 function renderInput(element) {
   element.innerHTML = '';
-  
+
+  let input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = '20:00';
+  input.classList.add('form-control', 'm-2');
+
+  let btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.addEventListener('click', () => {
+    let duration = input.value;
+    renderCountdown(element, duration);
+  });
 }
+
+/*---------- Countdown View ----------*/
 
 function renderCountdown(element, duration) {
   element.innerHTML = '';
   
-  // div: displays remaining time (mm:ss)
+  // timer div: displays remaining time in (mm:ss) format
   const timer = document.createElement('div');
   timer.id = 'timer';
   timer.classList.add('mt-5');
   timer.innerText = duration+':00';
 
-  const t = new Timer(timer);
+  // timer object: manages timer operations and countdown
+  const t = new Timer(timer, duration);
 
-  // div: container for controls/buttons
+  // constrols div: flex container for buttons
   const controls = document.createElement('div');
   controls.id = 'timer-controls';
   controls.classList.add('d-inline-flex', 'flex-row', 'justify-content-center', 'mb-3');
@@ -66,19 +89,19 @@ function renderCountdown(element, duration) {
   controls.appendChild(playButton);
 
   playButton.addEventListener('click', () => {
-
+    t.start();
   });
 
   // pause button
   const pauseButton = document.createElement('a');
-  playButton.id = 'pause-btn';
-  playButton.classList.add('btn', 'mx-1');
-  playButton.href = '#';
-  playButton.innerText = '⏸';
+  pauseButton.id = 'pause-btn';
+  pauseButton.classList.add('btn', 'mx-1');
+  pauseButton.href = '#';
+  pauseButton.innerHTML = 'I I';
   controls.appendChild(pauseButton);
 
   pauseButton.addEventListener('click', () => {
-
+    t.pause();
   });
 
   // reset button
@@ -89,21 +112,21 @@ function renderCountdown(element, duration) {
   resetButton.innerText = '↺';
   controls.appendChild(resetButton);
 
+  // cancels the running timer and brings up menu again
   resetButton.addEventListener('click', () => {
-
+    t.pause();
+    renderInput(element);
   });
 
-  // append time and controls to document
+  // append divs to document, start timer
   element.appendChild(timer);
   element.appendChild(controls);
-
-  // start the timer
-  t.start(duration);
+  t.start();
 }
 
 function renderTimer(element) {
   element.innerHTML = '';
-
+  renderCountdown(element, 1);
 }
 
 export default renderTimer;
